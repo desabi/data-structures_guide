5b23060c97ae7cd9060191b1ad53242dd7521655
desabi <desabi@live.com>
2025-04-26 19:22:27 -0600

    feat: udemy-scott-barrett, recursive binary search trees

--- udemy-scott-barrett/src/main/java/com/desabisc/ds/guide/scottbarrett/trees/BinarySearchThree.java
+++ udemy-scott-barrett/src/main/java/com/desabisc/ds/guide/scottbarrett/trees/BinarySearchThree.java
@@ -47,6 +47,33 @@ public class BinarySearchThree {
 
   }
 
+  /**
+   * Recursive version for insert method.
+   *
+   * @param currentNode the given node
+   * @param value the given value
+   * @return a given Node instance
+   */
+  private Node rInsert(Node currentNode, int value) {
+    if (currentNode == null) {
+      return new Node(value);
+    }
+
+    if (value < currentNode.value) {
+      currentNode.left = rInsert(currentNode.left, value);
+    } else if (value > currentNode.value) {
+      currentNode.right = rInsert(currentNode.right, value);
+    }
+    return currentNode;
+  }
+
+  public void rInsert(int value) {
+    if (root == null) {
+      root = new Node(value);
+    }
+    rInsert(root, value);
+  }
+
   public boolean contains(int value) {
     Node temp = root;
     while (temp != null) {
@@ -61,4 +88,68 @@ public class BinarySearchThree {
     return false;
   }
 
+  /**
+   * Recursive version for contains method.
+   *
+   * @param currentNode the given node
+   * @param value the given value
+   * @return return a boolean value
+   */
+  private boolean rContains(Node currentNode, int value) {
+    if (currentNode == null) {
+      return false;
+    }
+
+    if (currentNode.value == value) {
+      return true;
+    }
+
+    if (value < currentNode.value) {
+      return rContains(currentNode.left, value);
+    } else {
+      return rContains(currentNode.right, value);
+    }
+
+  }
+
+  public boolean rContains(int value) {
+    return rContains(root, value);
+  }
+
+  private Node deleteNode(Node currentNode, int value) {
+    if (currentNode == null) {
+      return null;
+    }
+
+    if (value < currentNode.value) {
+      currentNode.left = deleteNode(currentNode.left, value);
+    } else if (value > currentNode.value) {
+      currentNode.right = deleteNode(currentNode.right, value);
+    } else {
+      if (currentNode.left == null && currentNode.right == null) {
+        return null;
+      } else if (currentNode.left == null) {
+        currentNode = currentNode.right;
+      } else if (currentNode.right == null) {
+        currentNode = currentNode.left;
+      } else {
+        int subTreeMin = minValue(currentNode.right);
+        currentNode.value = subTreeMin;
+        currentNode.right = deleteNode(currentNode.right, subTreeMin);
+      }
+    }
+
+    return currentNode;
+  }
+
+  public int minValue(Node currentNode) {
+    while (currentNode.left != null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
+  }
+
+  public void deleteNode(int value) {
+    deleteNode(root, value);
+  }
 }
--- udemy-scott-barrett/src/main/java/com/desabisc/ds/guide/scottbarrett/trees/BinarySearchThreeMain.java
+++ udemy-scott-barrett/src/main/java/com/desabisc/ds/guide/scottbarrett/trees/BinarySearchThreeMain.java
@@ -7,7 +7,11 @@ public class BinarySearchThreeMain {
   public static void main(String[] args) {
     //constructorEg();
     //insertEg();
-    containsEg();
+    //containsEg();
+    //containsRecursiveEg();
+    //insertRecursiveEg();
+    //minValueEg();
+    deleteNodeEg();
   }
 
   static void constructorEg() {
@@ -47,5 +51,70 @@ public class BinarySearchThreeMain {
 
     log.info("{}", myBst.contains(27));
     log.info("{}", myBst.contains(17));
+  }
+
+  static void containsRecursiveEg() {
+    log.info("##### Contains Recursive Eg #####");
+    BinarySearchThree myBst = new BinarySearchThree();
+
+    myBst.insert(47);
+    myBst.insert(21);
+    myBst.insert(76);
+    myBst.insert(18);
+    myBst.insert(27);
+    myBst.insert(52);
+    myBst.insert(82);
+
+    log.info("BST Contains 27: {}", myBst.rContains(27));
+    log.info("BST Contains 17: {}", myBst.rContains(17));
+  }
+
+  static void insertRecursiveEg() {
+    log.info("##### Insert Recursive Eg #####");
+    BinarySearchThree myBst = new BinarySearchThree();
+
+    myBst.rInsert(2);
+    myBst.rInsert(1);
+    myBst.rInsert(3);
+
+    log.info("Root: {}", myBst.root.value);
+    log.info("Root->Left: {}", myBst.root.left.value);
+    log.info("Root->Right: {}", myBst.root.right.value);
+  }
+
+  static void minValueEg() {
+    log.info("##### Min Value Eg #####");
+    BinarySearchThree myBst = new BinarySearchThree();
+
+    myBst.insert(47);
+    myBst.insert(21);
+    myBst.insert(76);
+    myBst.insert(18);
+    myBst.insert(27);
+    myBst.insert(52);
+    myBst.insert(82);
+
+    log.info("Root, min value: {}", myBst.minValue(myBst.root));
+    log.info("Right Node, min value: {}", myBst.minValue(myBst.root.right));
+  }
+
+  static void deleteNodeEg() {
+    log.info("##### Delete Node Eg #####");
+    BinarySearchThree myBst = new BinarySearchThree();
+
+    myBst.rInsert(2);
+    myBst.rInsert(1);
+    myBst.rInsert(3);
+
+    log.info("Root: {}", myBst.root.value);
+    log.info("Root->Left: {}", myBst.root.left.value);
+    log.info("Root->Right: {}", myBst.root.right.value);
+
+    myBst.deleteNode(2);
+
+    log.info("Root: {}", myBst.root.value);
+    log.info("Root->Left: {}", myBst.root.left.value);
+    log.info("Root->Right: {}", myBst.root.right);
+
   }
 }
